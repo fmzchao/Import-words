6a718adc76a443bcf3df5cad0959bd6c3f6eb779
xiaomaolvkkp <root@7jdg.com>
2016-08-17 11:02:29 +0800

    第一版完成

--- postdanci.go
+++ postdanci.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"./utils/logs"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"github.com/mozillazg/request"
+	"github.com/sdwolfe32/anirip/anirip"
+	"github.com/widuu/gojson"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"sort"
 	"strings"
 	"time"
 	"unicode"
-	//strconv  //json += "\\u"+strconv.FormatInt(int64(rint), 16) // json
-	//"io"
-	//"net/http"
-	// "net/http/cookiejar"
-	// "net/url"
-	//"os"
-	//"request"
 )
 
 const (
@@ -24,6 +24,144 @@ const (
 	pwd            string = "fuckyou"
 )
 
+func GetCookies() (result []*http.Cookie) {
+	formData := url.Values{
+		"name":   {"root@7jdg.com"},
+		"passwd": {"fuckyou"},
+	}
+	loginReqHeaders := http.Header{}
+	loginReqHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0")
+	loginReqHeaders.Add("referer", "http://langeasy.com.cn/denglu.action")
+	loginReqHeaders.Add("content-type", "application/x-www-form-urlencoded")
+	loginResponse, err := anirip.GetHTTPResponse("POST",
+		"http://langeasy.com.cn/login.action",
+		bytes.NewBufferString(formData.Encode()),
+		loginReqHeaders,
+		nil)
+	if err != nil {
+		fmt.Println("[anirip] GetHTTPResponse Error ...")
+		return
+	}
+
+	var logincookie = loginResponse.Cookies()
+	return logincookie
+}
+
+func post_danci(danci, jieshi string, mycookie []*http.Cookie) string {
+	formData := url.Values{
+		"newwordlist": {"{\"word\":\"" + danci + "\",\"course\":\"*\",\"wordidx\":\"*\",\"infoidx\":\"100\",\"selection\":\"*\",\"info\":\"" + jieshi + "\",\"opcode\":\"1\"}"},
+	}
+	PostReqHeaders := http.Header{}
+	PostReqHeaders.Add("Accept", "*/*")
+	PostReqHeaders.Add("Origin", "chrome-extension://cklfipcjofdnmdolnfngpmokdaejidim")
+	PostReqHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0")
+	PostReqHeaders.Add("content-type", "application/x-www-form-urlencoded")
+	PostReqHeaders.Add("DNT", "1")
+	PostResponse, err := anirip.GetHTTPResponse("POST",
+		"http://langeasy.com.cn/insertNewWord.action",
+		bytes.NewBufferString(formData.Encode()),
+		PostReqHeaders,
+		mycookie)
+	if err != nil {
+		fmt.Println("[anirip] GetHTTPResponse Error ...")
+		//return
+	}
+	defer PostResponse.Body.Close()
+	body, err := ioutil.ReadAll(PostResponse.Body)
+	if err != nil {
+		fmt.Println("ReadAll Response Err:", err)
+		//return
+	}
+	var rBody = string(body)
+	//fmt.Println(rBody)
+	if rBody == "null" {
+		return danci + "单词添加成功"
+	} else {
+		return danci + "单词添加失败"
+	}
+
+}
+
+func geturl(myurl string, mycookie []*http.Cookie) (result string) {
+	//var loadLexisList = "http://langeasy.com.cn/loadLexisList.action?strict=1&word=another"
+	showResponse, err := anirip.GetHTTPResponse("GET",
+		myurl,
+		nil,
+		nil,
+		mycookie)
+	if err != nil {
+		fmt.Println("[anirip] GetHTTPResponse Error ...")
+		return
+	}
+	defer showResponse.Body.Close()
+	body, err := ioutil.ReadAll(showResponse.Body)
+	if err != nil {
+		fmt.Println("ReadAll Response Err:", err)
+		return
+	}
+	var rBody = string(body)
+	return rBody
+}
+
+//取单词解释
+func loadLexisList(danci string, mycookie []*http.Cookie) (result string) {
+	respBody := geturl("http://langeasy.com.cn/loadLexisList.action?strict=1&word="+danci, mycookie)
+
+	//var dat map[string]interface{}{}
+	dat := map[string]interface{}{}
+	//fmt.Println(respBody)
+	if strings.Contains(respBody, "interpret") == false {
+		return "false"
+	} else {
+		//
+		json.Unmarshal([]byte(respBody), &dat)
+
+		danci_jieshi := gojson.Json(respBody).Getindex(1).Getindex(1).Get("interpret").Tostring() //&{map[from:en to:zh]}
+
+		//fmt.Println(danci_jieshi)
+		danci_jieshi = strings.Replace(danci_jieshi, "\n", "\\n", -1)
+		return danci_jieshi
+	}
+}
+func getNewWord(danci string, mycookie []*http.Cookie) bool {
+	respBody := geturl("http://langeasy.com.cn/getNewWord.action?word="+danci+"&infoidx=100", mycookie)
+	//respBody = gojson.Json(respBody).Getindex(1).Getindex(1).Get("id").Tostring()
+	// if gojson.Json(respBody).Getindex(1).Getindex(1).Get("id").Tostring() == "" {
+	// 	return false
+	// }
+
+	//fmt.Println(respBody)
+
+	if strings.Contains(respBody, "updatetime") {
+		return true
+	} else {
+		return false
+	}
+	//return true
+}
+
+func tianjia_danci(danci_str string, cookie []*http.Cookie) bool {
+
+	//重写
+	//logs.Logger.Info("查看 " + danci_str + " 是否在单词本")
+	var isnewword = getNewWord(danci_str, cookie)
+	if isnewword == false { //单词本里没有
+		//开始查找单词解释
+		jieshi := loadLexisList(danci_str, cookie)
+		if jieshi == "false" {
+			logs.Logger.Error(danci_str + " 单词不存在具体解释，请查正")
+			return false
+		} else {
+			//logs.Logger.Info(danci_str + " 不存在单词本里，正在添加")
+			tijian := post_danci(danci_str, jieshi, cookie)
+			logs.Logger.Info(tijian)
+			return true
+		}
+	} else {
+		logs.Logger.Info(danci_str + " 单词已存在单词本中")
+		return false
+	}
+}
 func isNumber(s string) bool {
 	s = strings.TrimSpace(s)
 	n := len(s)
@@ -93,14 +231,31 @@ func RemoveDuplicatesAndEmpty(a []string) (ret []string) {
 
 func HandingText(str string) []string {
 	var ret []string
-	str = strings.ToLower(str)                //转小写
-	str = strings.Replace(str, ".", " ", -1)  //删除点
-	str = strings.Replace(str, ",", " ", -1)  //删除逗号
-	str = strings.Replace(str, "\n", " ", -1) //删除换行
-	str = strings.Replace(str, "(", " ", -1)  //删除换行
-	str = strings.Replace(str, ")", " ", -1)  //删除换行
-	str = strings.Replace(str, ";", " ", -1)  //删除换行
-	str = strings.Replace(str, "\"", " ", -1) //删除换行
+	str = strings.ToLower(str)                      //转小写
+	str = strings.Replace(str, ".", " ", -1)        //删除点
+	str = strings.Replace(str, ",", " ", -1)        //删除逗号
+	str = strings.Replace(str, "\n", " ", -1)       //删除换行
+	str = strings.Replace(str, "(", " ", -1)        //删除换行
+	str = strings.Replace(str, ")", " ", -1)        //删除换行
+	str = strings.Replace(str, ";", " ", -1)        //删除换行
+	str = strings.Replace(str, "\"", " ", -1)       //删除换行
+	str = strings.Replace(str, "/", " ", -1)        //删除换行
+	str = strings.Replace(str, "'", " ", -1)        //删除换行
+	str = strings.Replace(str, "*", " ", -1)        //删除换行
+	str = strings.Replace(str, "-", " ", -1)        //删除换行
+	str = strings.Replace(str, "=", " ", -1)        //删除换行
+	str = strings.Replace(str, ":", " ", -1)        //删除换行
+	str = strings.Replace(str, "[", " ", -1)        //删除换行
+	str = strings.Replace(str, "]", " ", -1)        //删除换行
+	str = strings.Replace(str, "?", " ", -1)        //删除换行
+	str = strings.Replace(str, "‘", " ", -1)        //删除换行
+	str = strings.Replace(str, "’", " ", -1)        //删除换行
+	str = strings.Replace(str, "“", " ", -1)        //删除换行
+	str = strings.Replace(str, "”", " ", -1)        //删除换行
+	str = strings.Replace(str, "!", " ", -1)        //删除换行
+	str = strings.Replace(str, "_", " ", -1)        //删除换行
+	str = strings.Replace(str, "https://", " ", -1) //删除换行
+	str = strings.Replace(str, "http://", " ", -1)  //删除换行
 
 	all_danci := strings.Split(str, " ") //分割成数组
 	//fmt.Println(all_danci)
@@ -123,16 +278,12 @@ func HandingText(str string) []string {
 	return ret
 }
 
-//获取登录页面的cookie
-func getLoginPageCookies() (strCookies string) {
-	//strLoginUrl := `http://weibo.com/login.php`
-	_, strCookies = DoRequest(`GET`, login_url, ``, ``, ``, nil)
-	return
-}
 func main() {
 	str := `
-GitHub Terms of Service
-By using the GitHub.com web site ("Service"), or any services of GitHub, Inc ("GitHub"), you are agreeing to be bound by the following terms and conditions ("Terms of Service"). IF YOU ARE ENTERING INTO THIS AGREEMENT ON BEHALF OF A COMPANY OR OTHER LEGAL ENTITY, YOU REPRESENT THAT YOU HAVE THE AUTHORITY TO BIND SUCH ENTITY, ITS AFFILIATES AND ALL USERS WHO ACCESS OUR SERVICES THROUGH YOUR ACCOUNT TO THESE TERMS AND CONDITIONS, IN WHICH CASE THE TERMS "YOU" OR "YOUR" SHALL REFER TO SUCH ENTITY, ITS AFFILIATES AND USERS ASSOCIATED WITH IT. IF YOU DO NOT HAVE SUCH AUTHORITY, OR IF YOU DO NOT AGREE WITH THESE TERMS AND CONDITIONS, YOU MUST NOT ACCEPT THIS AGREEMENT AND MAY NOT USE THE SERVICES.
+Pompem is an open source exploit & vulnerability finder tool, designed to automate the search for Exploits and Vulnerability in the most important databases. Developed in Python, has a system of advanced search, that help the work of pen-testers and ethical hackers. In the current version, it performs searches in PacketStorm security,...
+
+Read the full post at darknet.org.uk
+
     `
 	t2 := time.Now()
 	charlotteWeb := HandingText(str)
@@ -141,11 +292,13 @@ By using the GitHub.com web site ("Service"), or any services of GitHub, Inc ("G
 	fmt.Print("End ....\n\n")
 	fmt.Println("去重用时:", time.Now().Sub(t2))
 	//获取登录页面cookies
-	cookies := getLoginPageCookies()
-	fmt.Print(cookies)
-	c := &http.Client{}
-	req := request.NewRequest(c)
-	resp, err := req.Get("http://httpbin.org/get")
-	defer resp.Body.Close() // **Don't forget close the response body**
-	j, err := resp.Json()
+	//var danci_str = "logger"
+	var cookie = GetCookies()
+	fmt.Println(cookie)
+
+	for i := 0; i < len(charlotteWeb); i++ {
+		//logs.Logger.Info(charlotteWeb[i])
+		tianjia_danci(charlotteWeb[i], cookie)
+	}
+
 }
